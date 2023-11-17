11ea0843423218e601f91772156192dd0a82a5a4
Ivan <ivan.otroshko228@gmail.com>
2023-11-17 14:02:55 +0300

    end

--- src/components/Characters/index.tsx
+++ src/components/Characters/index.tsx
@@ -1,85 +1,52 @@
 import style from "./character.module.css";
 
 export const Character = () => {
+    const characters = [
+        {
+            name: "Джеймс Ганн",
+            image: "https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/0b0cba07-d80b-4a81-9602-005c5caacbcf/280x420"
+        },
+        {
+            name: "Роберт Дауни мл",
+            image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/badcb124-2d8c-455b-a8f6-ffd7459fc3be/280x420"
+        },
+        {
+            name: "Крис Эванс",
+            image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/0357ad10-8f5a-463e-86a6-42f5f6c4928c/280x420"
+        },
+        {
+            name: "Джон Фавро",
+            image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/bbb0dc5c-b2f9-4bfd-9512-26f214c39347/280x420"
+        },
+        {
+            name: "Крис Хемсворт",
+            image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/23f94a27-bd93-4074-add5-db22e54e0833/280x420"
+        },
+        {
+            name: "Кларк Грегг",
+            image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/6eda6c5a-6aff-474e-b374-96e2485d1a5b/280x420"
+        },
+        {
+            name: "Том Хиддлстон",
+            image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/34ece4ec-ae18-4489-a31c-eda0f610d48b/280x420"
+        },
+        {
+            name: "Сэмюэл Л. Джексон",
+            image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/2c4a3629-917c-431e-864e-c6e03b1b8b3a/280x420"
+        }
+    ];
+
     return (
         <div className={style.container}>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/0b0cba07-d80b-4a81-9602-005c5caacbcf/280x420" alt="Джеймс Ганн" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Джеймс Ганн
-                    </h2>
+            {characters.map((character, index) => (
+                <div className={style.card} key={index}>
+                    <img className={style.imgmovi} src={character.image} alt={character.name} />
+                    <div className={style.discription}>
+                        <h2>{character.name}</h2>
+                    </div>
                 </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/badcb124-2d8c-455b-a8f6-ffd7459fc3be/280x420" alt="Роберт Дауни мл" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Роберт Дауни мл
-                    </h2>
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/0357ad10-8f5a-463e-86a6-42f5f6c4928c/280x420" alt="Крис Эванс" />
-                <div className={style.discription}>
-
-                    <h2>
-                    Крис Эванс
-                    </h2>
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/bbb0dc5c-b2f9-4bfd-9512-26f214c39347/280x420" alt="Джон фавро" />
-                <div className={style.discription}>
-
-                    <h2>
-                    Джон Фавро
-                    </h2>
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/23f94a27-bd93-4074-add5-db22e54e0833/280x420" alt="Крис Хемсворт" />
-                <div className={style.discription}>
-
-                    <h2>
-                    Крис Хемсворт
-                    </h2>
-                </div>
-
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/6eda6c5a-6aff-474e-b374-96e2485d1a5b/280x420" alt="Кларк Грегг" />
-                <div className={style.discription}>
-
-                    <h2>
-                    Кларк Грегг
-                    </h2>
-                </div>
-
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/34ece4ec-ae18-4489-a31c-eda0f610d48b/280x420" alt="Том Хиддлстон" />
-                <div className={style.discription}>
-
-                    <h2>
-                    Том Хиддлстон
-                    </h2>
-                </div>
-
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/2c4a3629-917c-431e-864e-c6e03b1b8b3a/280x420" alt="Сэмюэл Л. Джексон" />
-                <div className={style.discription}>
-
-                    <h2>
-                    Сэмюэл Л. Джексон
-                    </h2>
-                </div>
-
-            </div>
-
+            ))}
         </div>
     );
 };
+
--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,232 +1,112 @@
 import style from "./card.module.css";
 
 export const Movie = () => {
+    const movies = [
+        {
+            title: "Thor Ragnarok",
+            year: 2017,
+            image: "https://i.playground.ru/p/KRVJg9bYBNlEE4TphOLS0g.jpeg"
+        },
+        {
+            title: "Captain Marvel",
+            year: 2019,
+            image: "https://www.kino-teatr.ru/movie/posters/big/3/129673.jpg"
+        },
+        {
+            title: "Avengers: End Game",
+            year: 2019,
+            image: "https://kvmarvel.ru/wp-content/uploads/2019/01/avengers-endgame-final-poster.jpg"
+        },
+        {
+            title: "Avengers",
+            year: 2012,
+            image: "https://www.kino-teatr.ru/movie/poster/42608/62050.jpg"
+        },
+        {
+            title: "Doctor Strange",
+            year: 2022,
+            image: "https://img.championat.com/i/z/u/1649251431268829557.jpg"
+        },
+        
+        {
+            title: "Black Panther",
+            year: 2018,
+            image: "https://www.film.ru/sites/default/files/filefield_paths/9364801-1001255.jpg" 
+        },
+        {
+            title: "Spider Man",
+            year: 2018,
+            image: "https://ic.pics.livejournal.com/madmundt/61253376/142296/142296_600.jpg"
+        },
+        {
+            title: "Eternals",
+            year: 2021,
+            image: "https://www.kinonews.ru/insimgs/2021/poster/poster103328_1.jpg"
+        }
+        ,
+        {
+            title: " Iron Man 3",
+            year: 2013,
+            image: "https://www.kinonews.ru/insimgs/poster/poster30068_3.jpg"
+        }
+        ,
+        {
+            title: "Morbius",
+            year: 2022,
+            image: "https://horrorzone.ru/uploads/_movies/75750/morbius-march.jpg"
+        }
+        ,
+        {
+            title: "Shang-Chi",
+            year: 2022,
+            image: "https://funduk.ua/upload/iblock/d78/d788804d144fcd8875c12cbecada2bea.jpg"
+        }
+        ,
+        {
+            title: "Thor",
+            year: 2011,
+            image: "https://i.pinimg.com/236x/6b/7e/1c/6b7e1c0cbd64a376097b30942113a7eb--thor-dvd-superhero-movies.jpg"
+        }
+        ,
+        {
+            title: "Thor: Love and Thunder",
+            year: 2022,
+            image: "https://upload.wikimedia.org/wikipedia/ru/9/92/%D0%A2%D0%BE%D1%80_%D0%9B%D1%8E%D0%B1%D0%BE%D0%B2%D1%8C_%D0%B8_%D0%B3%D1%80%D0%BE%D0%BC_%D0%BF%D0%BE%D1%81%D1%82%D0%B5%D1%80.jpg"
+        }
+        ,
+        {
+            title: "First Avenger",
+            year: 2011,
+            image: "https://www.kino-teatr.ru/movie/posters/big/8/104988.jpg"
+        }
+        ,
+        {
+            title: "Hulk",
+            year: 2003,
+            image: "https://cdn.oboi7.com/static/images/m/c6/45/c645281f0b64d8d4736f51a6937f3295db223340.jpg"
+        }
+        ,
+        {
+            title: "Iron Man",
+            year: 2008,
+            image: "https://www.kino-teatr.ru/movie/posters/big/4/25624.jpg"
+        }
+        
+        
+    ];
+
     return (
         <div className={style.container}>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://i.playground.ru/p/KRVJg9bYBNlEE4TphOLS0g.jpeg" alt="Thor Ragnarok" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Thor Ragnarok
-                    </h2>
-                    <p>
-                        2017
-                    </p>
+            {movies.map((movie, index) => (
+                <div key={index} className={style.card}>
+                    <img className={style.imgmovi} src={movie.image} alt={movie.title} />
+                    <div className={style.discription}>
+                        <h2>{movie.title}</h2>
+                        <p>{movie.year}</p>
+                    </div>
                 </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://www.kino-teatr.ru/movie/posters/big/3/129673.jpg" alt="Captain Marvel" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Captain Marvel
-                    </h2>
-                    <p>
-                        2019
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://kvmarvel.ru/wp-content/uploads/2019/01/avengers-endgame-final-poster.jpg" alt="Avengers: End Game" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Avengers: End Game
-                    </h2>
-                    <p>
-                        2019
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://www.kino-teatr.ru/movie/poster/42608/62050.jpg" alt="Avengers" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Avengers
-                    </h2>
-                    <p>
-                        2012
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://img.championat.com/i/z/u/1649251431268829557.jpg" alt="Doctor Strange" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Doctor Strange
-                    </h2>
-                    <p>
-                        2022
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://www.film.ru/sites/default/files/filefield_paths/9364801-1001255.jpg" alt="Black Panther" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Black Panther
-                    </h2>
-                    <p>
-                        2018
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://ic.pics.livejournal.com/madmundt/61253376/142296/142296_600.jpg" alt="Spider Man" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Spider Man
-                    </h2>
-                    <p>
-                        2018
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src=" https://www.kinonews.ru/insimgs/2021/poster/poster103328_1.jpg" alt="Eternals" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Eternals
-                    </h2>
-                    <p>
-                        2021
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://www.kinonews.ru/insimgs/poster/poster30068_3.jpg" alt="Iron Man 3" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Iron Man 3
-                    </h2>
-                    <p>
-                        2013
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://horrorzone.ru/uploads/_movies/75750/morbius-march.jpg" alt="Morbius" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Morbius
-                    </h2>
-                    <p>
-                        2022
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://funduk.ua/upload/iblock/d78/d788804d144fcd8875c12cbecada2bea.jpg" alt="Shang-Chi" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Shang-Chi
-                    </h2>
-                    <p>
-                        2022
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://i.pinimg.com/236x/6b/7e/1c/6b7e1c0cbd64a376097b30942113a7eb--thor-dvd-superhero-movies.jpg" alt="Thor" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Thor
-                    </h2>
-                    <p>
-                        2011
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://upload.wikimedia.org/wikipedia/ru/9/92/%D0%A2%D0%BE%D1%80_%D0%9B%D1%8E%D0%B1%D0%BE%D0%B2%D1%8C_%D0%B8_%D0%B3%D1%80%D0%BE%D0%BC_%D0%BF%D0%BE%D1%81%D1%82%D0%B5%D1%80.jpg
-" alt="Thor: Love and Thunder" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Thor: Love and Thunder
-                    </h2>
-                    <p>
-                        2022
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://www.kino-teatr.ru/movie/posters/big/8/104988.jpg" alt="First Avenger" />
-                <div className={style.discription}>
-
-                    <h2>
-                        First Avenger
-                    </h2>
-                    <p>
-                        2011
-                    </p>
-
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://cdn.oboi7.com/static/images/m/c6/45/c645281f0b64d8d4736f51a6937f3295db223340.jpg" alt=" Hulk" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Hulk
-                    </h2>
-                    <p>
-                        2003
-                    </p>
-
-                </div>
-            </div>
-            <div className={style.card}>
-                <img className={style.imgmovi} src="https://www.kino-teatr.ru/movie/posters/big/4/25624.jpg" alt="Iron Man" />
-                <div className={style.discription}>
-
-                    <h2>
-                        Iron Man
-                    </h2>
-                    <p>
-                        2008
-                    </p>
-
-
-                </div>
-            </div>
+            ))}
         </div>
-
-
     );
 };
+
